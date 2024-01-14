dbf1407777a308ecd60cd5dd48db062c4c058568
Gihoon Lim <knv429@naver.com>
2024-01-14 22:53:37 +0900

    reset

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,46 +1,18 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-// enum은 기본적으로 0,1,2와 같은 숫자로 저장되기 때문에 문자열로 쓸거면 따로 값을 넣어줘
-
-export interface IToDo {
-  text: string;
-  category: Categories;
-  id: number;
-}
-
-const localStorageEffect =
-  (key: string) =>
-  ({ setSelf, onSet }: any) => {
-    const savedToDos = localStorage.getItem(key);
-    if (savedToDos !== null) {
-      setSelf(JSON.parse(savedToDos));
-    }
-    onSet((toDos: any, _: any, isReset: boolean) => {
-      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(toDos));
-    });
-  };
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-  effects: [localStorageEffect("toDos")],
-});
-
-export const toDoSelector = selector({
-  key: "todoSelector",
+export const hourSelector = selector<number>({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
